9ff5d5555f9946847c00364179f235abb0e7314f
Rocky <rockyrider1080@gmail.com>
2022-12-25 11:13:09 -0600

    Variables

---
+++ Practice/Variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 25
+	var height float64 = 5.11
+	var weight float64 = 130.5
+	fmt.Println("My age is:", age)
+	fmt.Println("My weight is:", weight)
+	fmt.Println("My height is:", height)
+}
